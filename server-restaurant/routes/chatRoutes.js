cccec90289a7237678ddf584b892a4469e2bf6ee
Prakash Chandra sahoo <110497983+prakasham-om@users.noreply.github.com>
2025-06-21 18:15:15 +0530

    Update chatRoutes.js

--- server-restaurant/routes/chatRoutes.js
+++ server-restaurant/routes/chatRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const chatController = require("../controllers/chatController");
+const chatController = require("../controllers/chatControler");
 
 // GET /api/messages?user1=email1&user2=email2
 router.get("/", chatController.getMessages);
