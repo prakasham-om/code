9a394249ea007a5d2d05f5e28a6896c007d90a54
Prakash Chandra sahoo <110497983+prakasham-om@users.noreply.github.com>
2025-06-21 16:47:58 +0530

    Update Chat.js

--- server-restaurant/model/Chat.js
+++ server-restaurant/model/Chat.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const { encrypt, decrypt } = require("../utils/cryptoUtil");
+const { encrypt, decrypt } = require("../util/cryptoUtil");
 
 const messageSchema = new mongoose.Schema({
   sender: { type: String, required: true },
