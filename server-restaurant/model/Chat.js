51b4ff9698f561384694388b3e2d102df6acdf79
Prakash Chandra sahoo <110497983+prakasham-om@users.noreply.github.com>
2025-06-29 17:59:14 +0530

    Update Chat.js

--- server-restaurant/model/Chat.js
+++ server-restaurant/model/Chat.js
@@ -1,33 +1,10 @@
 const mongoose = require("mongoose");
-const { encrypt, decrypt } = require("../util/cryptoUtil");
 
-const messageSchema = new mongoose.Schema({
+const chatSchema = new mongoose.Schema({
   sender: { type: String, required: true },
   receiver: { type: String, required: true },
   encryptedMessage: { type: String, required: true },
-  timestamp: { type: Date, default: Date.now },
+  timestamp: { type: Date, default: Date.now }
 });
 
-// Pre-save hook to encrypt message
-messageSchema.pre("save", function(next) {
-  if (this.isModified("message")) {
-    this.encryptedMessage = encrypt(this.message);
-  }
-  next();
-});
-
-// Virtual for decrypted message
-messageSchema.virtual("decryptedMessage").get(function() {
-  return decrypt(this.encryptedMessage);
-});
-
-// Virtual for plain text message (for input)
-messageSchema.virtual("message")
-  .set(function(value) {
-    this._message = value;
-  })
-  .get(function() {
-    return this._message;
-  });
-
-module.exports = mongoose.model("Chat", messageSchema);
+module.exports = mongoose.model("Message", chatSchema);
